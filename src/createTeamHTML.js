2936a8c03c861b6c1d9946dd4705eaeb87abf8ac
Dylan <user@users-MBP-2.broadband>
2022-03-06 21:24:17 +0000

    update to string literal create team js

--- src/createTeamHTML.js
+++ src/createTeamHTML.js
@@ -44,7 +44,56 @@ const createManager = (manager) => {
       </div>
     </div>
     </div>
-`;
+    `;
+};
+
+const createEngineer = (engineer) => {
+    return `
+    <div class="flex justify-center m-5">
+        <div class="bg-white max-w-sm p-6 card-min-width">
+          <!-- circle -->
+          <div class="flex justify-center pb-5">
+            <div class="card-square-pink">
+              <img
+                class="justify-center"
+                src="....."
+                alt=""
+              />
+            </div>
+          </div>
+          <h5 class="text-black-700 text-xl font-bold mb-2 text-center">
+            ${engineer.name}
+          </h5>
+          <p
+            class="text-black bg-purple-400 font-bold mx-5 px-7 text-center"
+          >
+            Engineer
+          </p>
+          <!-- container -->
+          <div class="pt-4">
+            <!-- id -->
+            <div class="flex justify-between my-2">
+              <p class="font-bold text-base">ID:</p>
+              <p class="font-bold text-black-700 text-sm pl-8">${engineer.id}</p>
+            </div>
+            <!-- email -->
+            <div class="flex justify-between my-2">
+              <p class="font-bold text-base">Email:</p>
+              <p class="font-bold text-black-700 text-sm pl-8">
+              <a href="mailto:${engineer.email}">${engineer.email}</a>
+              </p>
+            </div>
+            <!-- GitHub Link -->
+            <div class="flex justify-between my-2">
+              <p class="font-bold text-base">GitHub:</p>
+              <p class="font-bold text-black-700 text-sm pl-8">
+                  <a href="https://github.com/${engineer.github}">${engineer.github}</a>
+                </p>
+            </div>
+          </div>
+        </div>
+      </div>  
+    `;
 };
 
 
