a5670bb2adb7c03e72a72e6fc2306215522907bb
Dylan <user@users-MBP-2.broadband>
2022-03-06 22:37:58 +0000

    Building out node.js question arrays

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+// Node dependencies
+const inquirer = require("inquirer");
+const fs = require("fs");
 
+// Link to Team Generation HTML
+const createTeamHTML = require("./src/teamGenHTML");
 
+// Array for the team
+const teamArray = [];
 
+const managerInfo = () => {
+    return inquirer
+        .prompt([
+        {
+            ty
+        }
 
-
+}
