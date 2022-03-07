00a90a794fa02fb1d02d57e95e976728414eb205
Dylan <user@users-MBP-2.broadband>
2022-03-07 21:33:36 +0000

    Updates to index.js and prompt array

--- index.js
+++ index.js
@@ -14,14 +14,21 @@ const createTeamHTML = require("./src/teamGenHTML");
 const teamArray = [];
 
 const managerInfo = () => {
+    console.log( `
+     -----------------------------------------------------------
+    |                                                           |
+    |                Time to build your team!                   |
+    |                                                           |
+      ----------------------------------------------------------
+    `);
     return inquirer
         .prompt([
         {
             name: "managersName",
             message: "Enter your managers name",
             type: "input",
-            validate: (input) => {
-                if (!input) {
+            validate: (managersName) => {
+                if (""(managersName)) {
                     return "You must enter your managers name";
                 } else {
                     return true;
@@ -33,9 +40,9 @@ const managerInfo = () => {
             name: "managersID",
             message: "Enter your managers ID",
             type: "input",
-            validate: (input) => {
-                if (""(!input)) {
-                    return "You must enter your managers name";
+            validate: (managersID) => {
+                if (""(managersID)) {
+                    return "You must enter your managers ID";
                 } else {
                     return true;
                 }
@@ -60,8 +67,8 @@ const managerInfo = () => {
             name: "managersOfficeNumber",
             message: "Enter your managers office number",
             type: "input",
-            validate: (input) => {
-                if (""(!input)) {
+            validate: (managersOfficeNumber) => {
+                if (""(managersOfficeNumber)) {
                     return "You must enter your managers office number";
                 } else {
                     return true;
@@ -69,7 +76,76 @@ const managerInfo = () => {
             },
         },
     ])
+  
+    .then((managerResponses) => {
+        const { managersName, managersID, managersEmail, managersOfficeNumber} = managerResponses;
+        const manager = new Manager( managersName, managersID, managersEmail, managersOfficeNumber);
+        
+        // Push data to array
+        teamArray.push(manager);
+        });
+};
+
+// Add employee after manager
+const addEmployee = () => {
+    console.log( `
+     -----------------------------------------------------------
+    |                                                           |
+    |                      Add an Employee!                     |
+    |                                                           |
+      ----------------------------------------------------------
+    `);
+    return inquirer
+        .prompt([
+            {
+                name: "role",
+                message: "Input the employee's role",
+                type: "list",
+                choices: ["Manager", "Engineer", "Intern"],
+            },
+            {
+                name: "name",
+                message: "What is the employee's name?",
+                type: "input",
+                validate: (name) => {
+                if (name) {
+                    return true;
+                } else {
+                    console.log("Input the employee's name");
+                    return false;
+                }
+            },
+            },
+            
+            {
+                name: "id",
+                message: "Please enter the employee's ID.",
+                type: "input",
+                validate: (ID) => {
+                if (""(ID)) {
+                    console.log("You must enter the employee's ID");
+                    return false;
+                } else {
+                    return true;
+                }
+            },
+            },
+            
+            {
+                name: "email",
+                essage: "Please enter the employee's email.",
+                type: "input",
+                validate: (email) => {
+                email = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+                if (email) {
+                    return true;
+                } else {
+                    console.log("You must enter an email");
+                    return false;
+                }
+            },
+            },
 
 
-
+        ])
 }
