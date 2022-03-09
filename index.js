2ef3ccbdf4eb5de6476e1725bdcf25ef961d600f
Dylan <user@users-MBP-2.broadband>
2022-03-09 00:23:03 +0000

    Still updating index.js and lib folder

--- index.js
+++ index.js
@@ -13,5 +13,52 @@ const createTeamHTML = require("./src/teamGenHTML");
 // Array for the team
 const teamArray = [];
 
+function runInquirer() {
+    const promptArray = [{
+        type: "input",
+        message: "What is your name?",
+        name: "name",
+        validate: (input) => {
+            if (!input) {
+                return "you must enter your managers name";
+            } else {
+                return true;
+            }
+        },
+    },
+    {
+        type: "input",
+        message: "What is your ID?",
+        name: "id",
+        validate: (ID) => {
+            if (isNaN(ID)) {
+                return "You must enter your ID";
+            } else {
+                return true;
+            }
+        },
+    },
+    {
+        name: "email",
+        message: "Enter your email",
+        type: "input",
+        validate: (email) => {
+            valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+            if (!valid) {
+                return "You must enter your email";
+            } else {
+                return true;
+            }
+        },
+    },
+    {
+        type: "list",
+        message: "What is your title",
+        choices: ["Manager", "Engineer", "Intern"],
+        name: "title"
+    }];
 
+    return inquirer
+        .prompt(promptArray);
+}
 
