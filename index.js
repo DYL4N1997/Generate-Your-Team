edff172f5dbaca5d87c388a259c854eaaa9cc361
Dylan <user@users-MBP-2.broadband>
2022-03-09 22:30:04 +0000

    Updates to index.js and lib

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 // Classes for the team
-const Manager = require("./lib/Manager");
-const Intern = require("./lib/Intern");
-const Engineer = require("./lib/Engineer");
+const Manager = require("./lib/manager");
+const Intern = require("./lib/intern");
+const Engineer = require("./lib/engineer");
 
 // Node dependencies
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-// Link to Team Generation HTML
-const createTeamHTML = require("./src/teamGenHTML");
+// // Link to Team Generation HTML
+// const createTeamHTML = require("./src/teamGenHTML");
 
-// Array for the team
-const teamArray = [];
+// // Array for the team
+// const teamArray = [];
 
 function runInquirer() {
     const promptArray = [{
@@ -62,3 +62,44 @@ function runInquirer() {
         .prompt(promptArray);
 }
 
+
+// Got here
+function runInquirerManager() {
+    const promptArray = [{
+        type: "input",
+        message: "What is your managers office number?",
+        name: "officeNumber",
+        validate: (managersOfficeNumber) => {
+            if (isNaN(managersOfficeNumber)) {
+                return "You must enter your managers office number";
+            } else {
+                return true;
+            }
+        },
+    }];
+
+    return inquirer
+        .prompt(promptArray);
+}
+
+function runInquirerEngineer() {
+    const promptArray = [{
+        type: "input",
+        message: "What yoyour github?",
+        name: "github"
+    }];
+
+    return inquirer
+        .prompt(promptArray);
+}
+
+function runInquirerIntern() {
+    const promptArray = [{
+        type: "input",
+        message: "What school do you attend?",
+        name: "school"
+    }];
+
+    return inquirer
+        .prompt(promptArray);
+}
